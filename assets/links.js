860c15baf5506431130e3d8d7b7bef9185d1d5dd
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2018-10-20 19:27:56 -0600

    Use the built in syntax highlighting

--- assets/links.js
+++ assets/links.js
@@ -1,4 +1,4 @@
-Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6')).forEach(function (el) {
+Array.from(document.querySelectorAll('h1, h2, h3')).forEach(function (el) {
   if (!el.id) return
   var a = document.createElement('a')
   a.href = '#' + el.id
